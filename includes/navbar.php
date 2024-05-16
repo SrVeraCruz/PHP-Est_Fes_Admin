ff3742481139533df3d525cd5974440c9746f39e
Dúdú Vera Cruz <130259216+SrVeraCruz@users.noreply.github.com>
2024-05-16 21:28:40 +0100

    Sommes updates

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,30 +2,26 @@
   <nav class="h-[4.7rem] p-4 py-3 flex justify-between bg-highlight items-center">
     <div class="">
       <h2 class="text-primary text-2xl font-semibold">
-        <a href="<?=isset($_SESSION['auth']) ? 'index.php' : '#'?>">Est Fes - Admin</a>
+        <a href="<?= isset($_SESSION['auth']) ? 'index.php' : '#' ?>">Est Fes - Admin</a>
       </h2>
     </div>
 
     <div class="flex items-center gap-4">
-      <?php if(isset($_SESSION['auth'])) : ?>
+      <?php if (isset($_SESSION['auth'])) : ?>
         <div class="relative">
-          <div 
-            class="h-[2.7rem] w-[2.7rem] shadow-lg border border-primary rounded-full bg-light cursor-pointer overflow-hidden group"
-          >
-            <?php if($_SESSION['auth_user']['user_img'] != null) : ?>
+          <div class="h-[2.7rem] w-[2.7rem] shadow-lg border border-primary rounded-full bg-light cursor-pointer overflow-hidden group">
+            <?php if ($_SESSION['auth_user']['user_img'] != null) : ?>
               <a href="setting.php">
-                <img class="h-full w-full object-cover" src="uploads/users/<?=$_SESSION['auth_user']['user_img']?>" alt="<?=$_SESSION['auth_user']['user_name']?>">
+                <img class="h-full w-full object-cover" src="uploads/users/<?= $_SESSION['auth_user']['user_img'] ?>" alt="<?= $_SESSION['auth_user']['user_name'] ?>">
               </a>
             <?php else : ?>
               <a href="setting.php">
                 <img class="h-full w-full object-cover" src="assets/img/user.png" alt="profil">
               </a>
             <?php endif ?>
-            <ul 
-              class="w-auto invisible opacity-0 absolute -bottom-16 -left-32 bg-light rounded-md shadow-md group-hover:opacity-100 group-hover:visible transition duration-200 ease-in-out text-primary text-sm"
-            >
+            <ul class="w-auto invisible opacity-0 absolute -bottom-16 -left-32 bg-light rounded-md shadow-md group-hover:opacity-100 group-hover:visible transition duration-200 ease-in-out text-primary text-sm">
               <li class="border-b-2 p-1 px-5 ">Logged in as:</li>
-              <li class="p-1 px-5 overflow-hidden text-ellipsis text-nowrap w-32 font-semibold"><?=$_SESSION['auth_user']['user_name']?></li>
+              <li class="p-1 px-5 overflow-hidden text-ellipsis text-nowrap w-32 font-semibold"><?= $_SESSION['auth_user']['user_name'] ?></li>
             </ul>
           </div>
         </div>
@@ -35,7 +31,7 @@
           <span class="inline-block w-6 h-0.5 rounded-md bg-dark"></span>
         </button>
       <?php endif ?>
-  </div>
-    
+    </div>
+
   </nav>
 </header>
--- index.php
+++ index.php
@@ -1,121 +1,119 @@
-<?php 
+<?php
 
-  include('middleware/authentication.php');
-  include('includes/header.php');
+include('middleware/authentication.php');
+include('includes/header.php');
 
 ?>
 
-  <?php include('includes/message.php')?>
+<?php include('includes/message.php') ?>
 
-  <section 
-    class="bg-white shadow-md p-4 pb-8 flex flex-col gap-4 rounded-md"
-  >
-    <div>
-      <h1 class=" text-[2rem] text-dark/75  font-semibold">Est-Admin</h1>
-      <ul class=" text-dark/65 flex gap-1">
-        <li>
-          <a href="index.php">Dashboard</a>
-        </li>
-      </ul>
-    </div>
+<section class="bg-white shadow-md p-4 pb-8 flex flex-col gap-4 rounded-md">
+  <div>
+    <h1 class=" text-[2rem] text-dark/75  font-semibold">Est-Admin</h1>
+    <ul class=" text-dark/65 flex gap-1">
+      <li>
+        <a href="index.php">Dashboard</a>
+      </li>
+    </ul>
+  </div>
 
-    <div class="grid grid-cols-4 xl:grid-cols-2 md:grid-cols-1 gap-5">
-      <div class="p-4 shadow-md bg-highlight/40 rounded-md min-h-28 hover:bg-highlight/75 hover:shadow-lg transition duration-200 ease-in-out hover:scale-[1.03] ">
-        <a href="user-view.php">
-          <div>
-            <h5 class=" text-base font-medium text-dark/80">Total Users</h5>
-            <?php 
-              $user_query = "SELECT id FROM users WHERE role_as <> '2'";
-              $user_result = mysqli_query($con,$user_query);
-
-              if($user_qte = mysqli_num_rows($user_result)) {
-                ?>
-                  <span class="text-dark/75 font-semibold text-2xl">
-                    <?=$user_qte?>
-                  </span>
-                <?php
-              } else {
-                ?>
-                  <span class="text-dark/75 font-semibold text-2xl">0</span>
-                <?php
-              }             
-            ?>
-          </div>
-        </a>
-      </div>
-      <div class="p-4 shadow-md bg-highlight/40 rounded-md min-h-28 hover:bg-highlight/75 hover:shadow-lg transition duration-200 ease-in-out hover:scale-105 ">
-        <a href="user-view.php">
-          <div>
-            <h5 class=" text-base font-medium text-dark/80">Total Blocked Users</h5>
-            <?php 
-              $b_user_query = "SELECT id FROM users WHERE status = '1'";
-              $b_user_result = mysqli_query($con,$b_user_query);
-
-              if($b_user_qte = mysqli_num_rows($b_user_result)) {
-                ?>
-                  <span class="text-dark/75 font-semibold text-2xl">
-                    <?=$b_user_qte?>
-                  </span>
-                <?php
-              } else {
-                ?>
-                  <span class="text-dark/75 font-semibold text-2xl">0</span>
-                <?php
-              }             
-            ?>
-          </div>
-        </a>
-      </div>
-      <div class="p-4 shadow-md bg-highlight/40 rounded-md min-h-28 hover:bg-highlight/75 hover:shadow-lg transition duration-200 ease-in-out hover:scale-105 ">
-        <a href="category-view.php">
-          <div>
-            <h5 class=" text-base font-medium text-dark/80">Total Categories</h5>
-            <?php 
-              $category_query = "SELECT id FROM categories WHERE status != '2'";
-              $category_result = mysqli_query($con,$category_query);
-
-              if($category_qte = mysqli_num_rows($category_result)) {
-                ?>
-                  <span class="text-dark/75 font-semibold text-2xl">
-                    <?=$category_qte?>
-                  </span>
-                <?php
-              } else {
-                ?>
-                  <span class="text-dark/75 font-semibold text-2xl">0</span>
-                <?php
-              }             
-            ?>
-          </div>
-        </a>
-      </div>
-      <div class="p-4 shadow-md bg-highlight/40 rounded-md min-h-28 hover:bg-highlight/75 hover:shadow-lg transition duration-200 ease-in-out hover:scale-105 ">
-      <a href="item-view.php">
+  <div class="grid grid-cols-4 xl:grid-cols-2 md:grid-cols-1 gap-5">
+    <div class="p-4 shadow-md bg-highlight/40 rounded-md min-h-28 hover:bg-highlight/75 hover:shadow-lg transition duration-200 ease-in-out hover:scale-[1.03] ">
+      <a href="user-view.php">
         <div>
-          <h5 class=" text-base font-medium text-dark/80">Total Items</h5>
-          <?php 
-            $item_query = "SELECT id FROM items";
-            $item_result = mysqli_query($con,$item_query);
+          <h5 class=" text-base font-medium text-dark/80">Total Users</h5>
+          <?php
+          $user_query = "SELECT id FROM users WHERE role_as <> '2'";
+          $user_result = mysqli_query($con, $user_query);
 
-            if($item_qte = mysqli_num_rows($item_result)) {
-              ?>
-                <span class="text-dark/75 font-semibold text-2xl">
-                  <?=$item_qte?>
-                </span>
-              <?php
-            } else {
-              ?>
-                <span class="text-dark/75 font-semibold text-2xl">0</span>
-              <?php
-            }             
+          if ($user_qte = mysqli_num_rows($user_result)) {
+          ?>
+            <span class="text-dark/75 font-semibold text-2xl">
+              <?= $user_qte ?>
+            </span>
+          <?php
+          } else {
+          ?>
+            <span class="text-dark/75 font-semibold text-2xl">0</span>
+          <?php
+          }
           ?>
         </div>
       </a>
     </div>
-  </section>
-    
-  <?php 
-  
-  include('includes/footer.php')
-  
-  ?>
+    <div class="p-4 shadow-md bg-highlight/40 rounded-md min-h-28 hover:bg-highlight/75 hover:shadow-lg transition duration-200 ease-in-out hover:scale-105 ">
+      <a href="user-view.php">
+        <div>
+          <h5 class=" text-base font-medium text-dark/80">Total Blocked Users</h5>
+          <?php
+          $b_user_query = "SELECT id FROM users WHERE status = '1'";
+          $b_user_result = mysqli_query($con, $b_user_query);
+
+          if ($b_user_qte = mysqli_num_rows($b_user_result)) {
+          ?>
+            <span class="text-dark/75 font-semibold text-2xl">
+              <?= $b_user_qte ?>
+            </span>
+          <?php
+          } else {
+          ?>
+            <span class="text-dark/75 font-semibold text-2xl">0</span>
+          <?php
+          }
+          ?>
+        </div>
+      </a>
+    </div>
+    <div class="p-4 shadow-md bg-highlight/40 rounded-md min-h-28 hover:bg-highlight/75 hover:shadow-lg transition duration-200 ease-in-out hover:scale-105 ">
+      <a href="category-view.php">
+        <div>
+          <h5 class=" text-base font-medium text-dark/80">Total Categories</h5>
+          <?php
+          $category_query = "SELECT id FROM categories WHERE status != '2'";
+          $category_result = mysqli_query($con, $category_query);
+
+          if ($category_qte = mysqli_num_rows($category_result)) {
+          ?>
+            <span class="text-dark/75 font-semibold text-2xl">
+              <?= $category_qte ?>
+            </span>
+          <?php
+          } else {
+          ?>
+            <span class="text-dark/75 font-semibold text-2xl">0</span>
+          <?php
+          }
+          ?>
+        </div>
+      </a>
+    </div>
+    <div class="p-4 shadow-md bg-highlight/40 rounded-md min-h-28 hover:bg-highlight/75 hover:shadow-lg transition duration-200 ease-in-out hover:scale-105 ">
+      <a href="item-view.php">
+        <div>
+          <h5 class=" text-base font-medium text-dark/80">Total Items</h5>
+          <?php
+          $item_query = "SELECT id FROM items";
+          $item_result = mysqli_query($con, $item_query);
+
+          if ($item_qte = mysqli_num_rows($item_result)) {
+          ?>
+            <span class="text-dark/75 font-semibold text-2xl">
+              <?= $item_qte ?>
+            </span>
+          <?php
+          } else {
+          ?>
+            <span class="text-dark/75 font-semibold text-2xl">0</span>
+          <?php
+          }
+          ?>
+        </div>
+      </a>
+    </div>
+</section>
+
+<?php
+
+include('includes/footer.php')
+
+?>
